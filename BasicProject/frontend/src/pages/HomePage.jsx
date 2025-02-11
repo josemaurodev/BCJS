6983fe2066c0798e046ce855658a0cd94aaa5d8f
Jose Mauro <josemauro.dev@gmail.com>
2025-02-11 12:13:39 -0300

    This project will end here. Not finished. Chakra UI to blame

--- BasicProject/frontend/src/pages/HomePage.jsx
+++ BasicProject/frontend/src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Container, VStack, Text, Heading } from "@chakra-ui/react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW="container.sm" py={12}>
+      <VStack spacing={8}>
+        <Heading as={"h1"} size={"2x1"} textAlign={"center"} mb={"8"}>
+          Current Products
+        </Heading>
+      </VStack>
+    </Container>
+  );
 };
 
 export default HomePage;
